26336f53c76e40233407f64373fa2393672636e6
aruzhannurash <166316831+aruzhannurash@users.noreply.github.com>
2025-04-22 23:12:16 +0500

    Update main.go practice10

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 
 	"github.com/aruzhannurash/quickbook/handlers"
-	"github.com/aruzhannurash/quickbook/models"
+	"github.com/aruzhannurash/quickbook/middlewares"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
@@ -18,30 +18,38 @@ func main() {
 		log.Fatal("Failed to connect to DB:", err)
 	}
 
-	db.AutoMigrate(&models.Specialist{}, &models.Client{}, &models.Appointment{})
-
 	h := &handlers.Handler{DB: db}
 	r := gin.Default()
 
-	api := r.Group("/api")
+	r.POST("/register", h.Register)
+	r.POST("/login", h.Login)
 
-	api.GET("/specialists", h.GetSpecialists)
-	api.GET("/specialists/:id", h.GetSpecialistByID)
-	api.POST("/specialists", h.CreateSpecialist)
-	api.PUT("/specialists/:id", h.UpdateSpecialist)
-	api.DELETE("/specialists/:id", h.DeleteSpecialist)
+	auth := r.Group("/api")
+	auth.Use(middlewares.JWTAuthMiddleware())
 
-	api.GET("/clients", h.GetClients)
-	api.GET("/clients/:id", h.GetClientByID)
-	api.POST("/clients", h.CreateClient)
-	api.PUT("/clients/:id", h.UpdateClient)
-	api.DELETE("/clients/:id", h.DeleteClient)
+	auth.GET("/specialists", h.GetSpecialists)
+	auth.GET("/specialists/:id", h.GetSpecialistByID)
+	auth.POST("/specialists", h.CreateSpecialist)
+	auth.PUT("/specialists/:id", h.UpdateSpecialist)
+	auth.DELETE("/specialists/:id", h.DeleteSpecialist)
 
-	api.GET("/appointments", h.GetAppointments)
-	api.GET("/appointments/:id", h.GetAppointmentByID)
-	api.POST("/appointments", h.CreateAppointment)
-	api.PUT("/appointments/:id", h.UpdateAppointment)
-	api.DELETE("/appointments/:id", h.DeleteAppointment)
+	auth.GET("/clients", h.GetClients)
+	auth.GET("/clients/:id", h.GetClientByID)
+	auth.POST("/clients", h.CreateClient)
+	auth.PUT("/clients/:id", h.UpdateClient)
+	auth.DELETE("/clients/:id", h.DeleteClient)
+
+	auth.GET("/appointments", h.GetAppointments)
+	auth.GET("/appointments/:id", h.GetAppointmentByID)
+	auth.POST("/appointments", h.CreateAppointment)
+	auth.PUT("/appointments/:id", h.UpdateAppointment)
+	auth.DELETE("/appointments/:id", h.DeleteAppointment)
+	auth.GET("/appointments/client/:client_id", h.GetAppointmentsByClient)
+	auth.GET("/appointments/specialist/:specialist_id", h.GetAppointmentsBySpecialist)
+
+	auth.POST("/reviews", h.CreateReview)
+	auth.GET("/reviews/specialist/:specialist_id", h.GetReviewsBySpecialist)
+	auth.GET("/reviews/client/:client_id", h.GetReviewsByClient)
 
 	log.Println("Server is running on http://localhost:8080")
 	r.Run(":8080")
